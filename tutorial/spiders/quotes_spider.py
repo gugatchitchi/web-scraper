c2ff4b4f12af3e5e6eaa4ac0308df58aae986502
Guga <gugatchitchinadze@gmail.com>
2020-09-23 20:55:01 +0400

    data is cleaned

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,7 +4,6 @@ import scrapy
 # to append json file ---->    scrapy crawl quotes --nolog -o quotes1.json 
 # to overwrite jsaon file ----> scrapy crawl quotes -t json --nolog -o "quotes1.json"
 # TODO overwrite does not work if you do not access different file first. dont know why.
-
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
@@ -17,10 +16,17 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for item in response.css('a.single-item'):
-            yield {
-                'text': item.css('h3.title::text').get(),
-                # 'descrip': item.css('p.description::text').get(),
-                'descrip': item.css('p.description::text').get(),
-                'date': item.css('p.date::text').get(),
-                'img': item.css('div.youtube-bg').attrib['style']
+
+            post_title      = item.css('h3.title::text').get()
+            post_descrip    = item.css('p.description::text').get()[4:-4].strip()
+            post_date       = item.css('p.date::text').get()
+            post_imgUrl     = item.css('div.youtube-bg').attrib['style'][22:-2]
+
+            data = {
+                'title': post_title,
+                'descrip': post_descrip,
+                'date': post_date, 
+                'imgUrl': post_imgUrl
             }
+
+            yield data
